95cfd5bdcb1d9ae96340fa042441928fa538b51a
Vyshakh Kandamath <kvyshakh@terpmail.umd.edu>
2017-01-17 23:41:44 -0500

    some setup

---
+++ background.js
@@ -0,0 +1,5 @@
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // Send a message to the active tab
+  chrome.tabs.create({url: 'main.html'});
+});
